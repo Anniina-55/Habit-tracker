e2992cd23aee5214edd0b3d6ddda8d1a087d7210
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-09 20:17:18 +0200

    Set up Retrofit instance and Weather API service

--- app/src/main/java/com/example/habit_tracker/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/habit_tracker/network/RetrofitInstance.kt
@@ -1,5 +1,17 @@
 package com.example.habit_tracker.network
 
-interface RetrofitInstance {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.weatherapi.com/v1/"
+
+    val api: WeatherAPIService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherAPIService::class.java)
+    }
 
 }
--- app/src/main/java/com/example/habit_tracker/network/WeatherAPI.kt
+++ app/src/main/java/com/example/habit_tracker/network/WeatherAPI.kt
@@ -1,5 +1,19 @@
 package com.example.habit_tracker.network
 
-interface WeatherAPI {
+import com.example.habit_tracker.model.Weather
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+data class WeatherResponse(
+    val current: Weather
+)
+
+interface WeatherAPIService {
+    @GET("current.json") // endpoint
+    suspend fun getWeather(
+        @Query("key") apiKey: String,
+        //@Query("q") city: String,
+        @Query("q") location: String,
+        @Query("lang") lang: String
+    ): WeatherResponse
 }
