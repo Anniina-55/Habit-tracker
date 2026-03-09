ec3b2ca145725c97c3a522ec20d03a322cb752be
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-09 20:22:26 +0200

    Configured Gradle to load weather API_KEY from local.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.util.Properties
+val localProperties = rootProject.file("local.properties")
+val props = Properties()
+if (localProperties.exists()) {
+    props.load(localProperties.inputStream())
+}
+val weatherApiKey = props.getProperty("WEATHER_API_KEY") ?: ""
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -18,6 +26,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // lue local.properties arvo
+        buildConfigField("String", "WEATHER_API_KEY", "\"$weatherApiKey\"")
+
     }
 
     buildTypes {
@@ -38,10 +50,16 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -53,6 +71,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
