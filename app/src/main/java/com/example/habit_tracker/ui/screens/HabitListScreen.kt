9e4dbea77ccf25752a41618cc7316c32ee7474d8
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-10 23:57:45 +0200

    Added progress-bar to be displayed and button to navigate to PlantScreen

--- app/src/main/java/com/example/habit_tracker/ui/screens/HabitListScreen.kt
+++ app/src/main/java/com/example/habit_tracker/ui/screens/HabitListScreen.kt
@@ -1,27 +1,36 @@
 package com.example.habit_tracker.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.habit_tracker.model.Habit
 import com.example.habit_tracker.model.WeekDay
+import com.example.habit_tracker.navigation.Screens
 import com.example.habit_tracker.ui.components.HabitList
+import com.example.habit_tracker.ui.components.ShowProgress
 import com.example.habit_tracker.viewModel.HabitTrackerViewModel
 
 // here HabitList composable is rendered and list of habits for specific day is fetched through ViewModel
@@ -29,55 +38,107 @@ import com.example.habit_tracker.viewModel.HabitTrackerViewModel
 fun HabitListScreen(
     selectedDay: String,
     habitViewModel: HabitTrackerViewModel,
+    navController: NavController,
     modifier: Modifier = Modifier
 ) {
     var newHabitName by remember { mutableStateOf("") }
 
-    Column(modifier = modifier.padding(16.dp)) {
-        Text(text = "Your habits for $selectedDay")
+    Text(
+        text = "Your habits for $selectedDay",
+        style = MaterialTheme.typography.titleLarge,
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    //Spacer(modifier = Modifier.height(30.dp))
 
-            TextField(
-                value = newHabitName,
-                onValueChange = { newHabitName = it },
-                placeholder = { Text("Enter new habit") },
-                singleLine = true,
-                maxLines = 1,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Text
-                ),
-                modifier = Modifier.weight(1f)
+    LazyColumn(
+        modifier = modifier.padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                TextField(
+                    value = newHabitName,
+                    onValueChange = { newHabitName = it },
+                    placeholder = { Text("Enter habit") },
+                    singleLine = true,
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Text
+                    ),
+                    modifier = Modifier.weight(1f),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color(0xFFFCF5FD),
+                        unfocusedContainerColor = Color(0xFFFCF5FD),
+                        focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                        unfocusedIndicatorColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+                Button(
+                    onClick = {
+                        if (newHabitName.isNotBlank()) {
+                            habitViewModel.addHabit(
+                                Habit(
+                                    habitName = newHabitName,
+                                    day = WeekDay.valueOf(selectedDay),
+                                    id = habitViewModel.habits.size,
+                                    completed = false,
+                                    deleted = false)
+                            )
+                            newHabitName = "" // clear text field
+                        }
+                    }
+                ) {
+                    Text("Add") // btn label
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            val habitsForDay = habitViewModel.getHabitsForDay(WeekDay.valueOf(selectedDay))
+            // onHabitCheckedChange and onHabitDelete are passed down to HabitList so it can modify the data
+            HabitList(
+                habits = habitsForDay,
+                onCheckedChange = { id, completed -> habitViewModel.toggleHabit(id, completed) },
+                onHabitDelete = { id -> habitViewModel.deleteHabit(id) },
+                modifier = modifier
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ShowProgress(
+                habitsViewModel = habitViewModel,
+                selectedDay = WeekDay.valueOf(selectedDay),
+                modifier = modifier.padding(horizontal = 16.dp)
+            )
+
             Button(
                 onClick = {
-                    if (newHabitName.isNotBlank()) {
-                        habitViewModel.addHabit(Habit(
-                            habitName = newHabitName,
-                            day = WeekDay.valueOf(selectedDay),
-                            id = habitViewModel.habits.size,
-                            completed = false,
-                            deleted = false
-                        ))
-                        newHabitName = ""
-                    }
-                }
-            ) {
-                Text("Add")
+                    // navigates to PlantScreen with selected day as argument
+                    navController.navigate("${Screens.PLANT}/$selectedDay")
+                },
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.secondary,
+                        contentColor = Color.Black)
+                )
+            {
+                Text(
+                    "View your daily plant",
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        val habitsForDay = habitViewModel.getHabitsForDay(WeekDay.valueOf(selectedDay))
-        // onHabitCheckedChange and onHabitDelete are passed down to HabitList so it can modify the data
-        HabitList(
-            habits = habitsForDay,
-            onCheckedChange = { id, completed -> habitViewModel.toggleHabit(id, completed) },
-            onHabitDelete = { id -> habitViewModel.deleteHabit(id) },
-            modifier = modifier
-        )
     }
 }
+
+
